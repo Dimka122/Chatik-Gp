bd97bee7dfddac250b5567daec373cc0da2958c3
Дмитрий Кассин <kassindima1@gmail.com>
2023-04-03 20:07:02 +0300

    пп

--- ChatGP/ChatGP/Form1.Designer.cs
+++ ChatGP/ChatGP/Form1.Designer.cs
@@ -28,17 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(306, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtUsername.Location = new System.Drawing.Point(306, 24);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(142, 20);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
             // 
             // label1
             // 
@@ -49,25 +55,77 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "имя";
             // 
-            // button1
+            // btnRegister
             // 
-            this.button1.Location = new System.Drawing.Point(454, 24);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "зарегистрироваться";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRegister.Location = new System.Drawing.Point(454, 24);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(125, 23);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "зарегистрироваться";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вопрос";
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(13, 62);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(566, 45);
+            this.txtQuestion.TabIndex = 4;
+            this.txtQuestion.TextChanged += new System.EventHandler(this.txtQuestion_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ответ";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(16, 157);
+            this.txtAnswer.Multiline = true;
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(563, 184);
+            this.txtAnswer.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(176, 114);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(239, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "задать вопрос";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(586, 353);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtUsername);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,9 +133,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
--- ChatGP/ChatGP/Form1.cs
+++ ChatGP/ChatGP/Form1.cs
@@ -3,18 +3,130 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace ChatGP
 {
+    
     public partial class Form1 : Form
     {
+        private List<QAPair> qaPairs;
+        string username;
         public Form1()
         {
             InitializeComponent();
+            LoadQAPairs();
         }
+        private void LoadQAPairs()
+        {
+            qaPairs = new List<QAPair>();
+            qaPairs.Add(new QAPair { Question = "Как дела?", Answer = "Хорошо, спасибо!" });
+            qaPairs.Add(new QAPair { Question = "Какой сегодня день?", Answer = "Сегодня " + DateTime.Today.ToString("dd.MM.yyyy") });
+            qaPairs.Add(new QAPair { Question = "Как тебя зовут?", Answer = "Меня зовут Бот" });
+            qaPairs.Add(new QAPair { Question = "Какие новости?", Answer = "Пока нет новостей" });
+        }
+
+        public class QAPair
+        {
+            public string Question { get; set; }
+            public string Answer { get; set; }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string question = txtQuestion.Text; 
+
+            string answer = FindAnswer(question); 
+
+            
+            txtAnswer.Text = answer;
+        }
+        private string FindAnswer(string question)
+        {
+            
+            QAPair qaPair = qaPairs.FirstOrDefault(qa => qa.Question.ToLower() == question.ToLower());
+
+            if (qaPair != null)
+            {
+                return qaPair.Answer; 
+            }
+            else
+            {
+                return "Извините, я не понимаю вас"; 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (IsUserRegistered())
+            {
+                MessageBox.Show("Добро пожаловать, " + username + "!");
+                txtQuestion.Enabled = true;
+                btnSend.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, зарегистрируйтесь.");
+                txtUsername.Enabled = true;
+            }
+        }
+
+        private void txtQuestion_TextChanged(object sender, EventArgs e)
+        {
+            if (txtQuestion.Text != "")
+            {
+                btnSend.Enabled = true;
+            }
+            else
+            {
+                btnSend.Enabled = false;
+            }
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUsername.Text != "")
+            {
+                btnRegister.Enabled = true;
+            }
+            else
+            {
+                btnRegister.Enabled = false;
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            username = txtUsername.Text;
+            SaveUsernameToFile();
+            MessageBox.Show("Пользователь зарегистрирован");
+            txtQuestion.Enabled = true;
+            btnSend.Enabled = true;
+        }
+        private void SaveUsernameToFile()
+        {
+            string filePath = "username.txt";
+            File.WriteAllText(filePath, username);
+        }
+
+        private bool IsUserRegistered()
+        {
+            string filePath = "username.txt";
+            if (File.Exists(filePath))
+            {
+                username = File.ReadAllText(filePath);
+                if (username != "")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
     }
 }
